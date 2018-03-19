90425134a1272a824ccc0bbf8217599e81dbf0d5
Dankiest Raft <daniel.yahalom@gmail.com>
2018-03-19 10:15:09 -0500

    Finally fixed jumping hurray I'm so happy right now but I must be emotionless because it's school and I don't want to embarres myself.

--- Assets/Scripts/Player/movement.cs
+++ Assets/Scripts/Player/movement.cs
@@ -7,9 +7,13 @@ public class movement : MonoBehaviour {
 	public float jumpHeight;
 	public float movementSpeed;
 
+	bool isGrounded;
 	void Update()
 	{
-		if(Input.GetButtonDown("Jump"))
+		/*prevY = currY;
+		currY = transform.position.y;
+		yDelta = currY-prevY;*/
+		if(Input.GetButtonDown("Jump") && isGrounded)
 			StartCoroutine(Jump());
 	}
 
@@ -18,9 +22,28 @@ public class movement : MonoBehaviour {
 		yield return new WaitForFixedUpdate();
 		GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight);
 	}
-
+	Ray2D[] rays = new Ray2D[2]; 
 	void FixedUpdate()
 	{
-		GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		Collider2D col = GetComponent<Collider2D>();
+
+		rays[0] = new Ray2D(new Vector2(col.bounds.min.x, transform.position.y), Vector2.down);
+		rays[1] = new Ray2D(new Vector2(col.bounds.max.x, transform.position.y), Vector2.down);
+		
+		isGrounded = false;
+		foreach(Ray2D ray in rays)
+		{
+			RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, col.bounds.extents.y+0.05f, 127);
+			
+			if(hit.collider != null)
+			{
+				isGrounded = true;
+				Debug.Log(hit.collider.name);
+			}
+		}
 	}
+	
 }

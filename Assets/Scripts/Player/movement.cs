97fa902e05108234ae7f0e68e93269e0678fd19c
Dankiest Raft <daniel.yahalom@gmail.com>
2018-03-18 22:41:43 -0500

    adding the actual Unity project
    It's kind of annoying you have to create the repository first and than create a Unity project here...

---
+++ Assets/Scripts/Player/movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	public float jumpHeight;
+	public float movementSpeed;
+
+	void Update()
+	{
+		if(Input.GetButtonDown("Jump"))
+			StartCoroutine(Jump());
+	}
+
+	IEnumerator Jump()
+	{
+		yield return new WaitForFixedUpdate();
+		GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight);
+	}
+
+	void FixedUpdate()
+	{
+		GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+}
